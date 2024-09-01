b44eb7599ad66241b4dffe3f5a83cbd49d320a33
JijoAndrews <jijoandrews07@gmail.com>
2024-09-01 07:27:32 +0530

    proper importing libaries

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap/dist/css/bootstrap.css';
 import App from '../components/App'
 
-import 'bootstrap-icons/font/bootstrap-icons.css';
+// import 'bootstrap-icons/font/bootstrap-icons.css';
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 // import App from './App.jsx'
 // import './index.css'
